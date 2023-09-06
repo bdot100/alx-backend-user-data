35638a4c670702360f82d53ecd1beeb1952a7954
bdot100 <bidemiokunade@gmail.com>
2023-09-06 23:27:29 +0100

    Request Validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -29,8 +29,8 @@ def before_request():
         return
 
     excluded_paths = [
-        '/api/v1/status/', 
-        '/api/v1/unauthorized/', 
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
         ]
     if auth.require_auth(request.path, excluded_paths):
