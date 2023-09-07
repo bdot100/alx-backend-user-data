d14860cc2df8c102c6ca87137a6835d1a244bc2f
bdot100 <bidemiokunade@gmail.com>
2023-09-07 12:47:12 +0100

    Allow Passwords with colon

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -85,14 +85,18 @@ class BasicAuth(Auth):
             'password123').
         """
         if decoded_base64_authorization_header is None:
-            return None, None
+            return (None, None)
         if not isinstance(decoded_base64_authorization_header, str):
-            return None, None
-        if ':' not in decoded_base64_authorization_header:
-            return None, None
+            return (None, None)
+        # if ':' not in decoded_base64_authorization_header:
+        #     return (None, None)
+        parts = decoded_base64_authorization_header.split(':', 1)
+        if len(parts) < 2:
+            return (None, None)
 
-        email, password = decoded_base64_authorization_header.split(':', 1)
-        return email, password
+        email, password = parts[0], parts[1]
+        # decoded_base64_authorization_header.split(':', 1)
+        return (email, password)
 
     def user_object_from_credentials(
         self, user_email: str, user_pwd: str
