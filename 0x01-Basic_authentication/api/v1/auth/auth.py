53ad781d5f6e7b79cf244ea9938ef261b1e17f41
bdot100 <bidemiokunade@gmail.com>
2023-09-07 12:58:03 +0100

    Allow Passwords with colon

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -25,17 +25,19 @@ class Auth:
         """
         if path is None:
             return True
-
-        if excluded_paths is None or not excluded_paths:
+        elif excluded_paths is None or not excluded_paths:
             return True
-
-        for p in excluded_paths:
-            if p.endswith('/'):
-                p = p[:-1]  # Remove the trailing slash
-
-            if path == p or path.startswith(p + '/'):
-                return False
-
+        elif path in excluded_paths:
+            return False
+        else:
+            for p in excluded_paths:
+                if p.startswith(path):
+                    return False
+                if path.startswith(p):
+                    return False
+                if p[-1] == "*":
+                    if path.startswith(p[:-1]):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:
