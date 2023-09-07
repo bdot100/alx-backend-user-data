8c088f147c6469981a5e28ef0689f9d6a03399ab
bdot100 <bidemiokunade@gmail.com>
2023-09-07 12:04:19 +0100

    Retrieve User Object Function

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ import base64
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -92,3 +93,36 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> TypeVar('User'):
+        """
+        This Function Get a User instance based on email and
+        password credentials.
+
+        Args:
+            user_email (str): The user's email.
+            user_pwd (str): The user's password.
+
+        Returns:
+            User: The User instance if credentials are valid,
+            else None.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        # Use the User class's search method to look up users by email
+        users = User.search({'email': user_email})
+
+        if not users or users == []:
+            return None  # No user with the provided email found
+
+        # Check if the password is valid for any of the found users
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None  # No user with a matching password found
