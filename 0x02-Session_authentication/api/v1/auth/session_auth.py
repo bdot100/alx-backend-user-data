09ccf79ef823bd8adfac401146463b87dcd7aba4
bdot100 <bidemiokunade@gmail.com>
2023-09-07 17:48:59 +0100

    Session Auth Implemented

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This class will manage the API authentication through
+session auth.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Class to manage our api through session
+    authentication
+    """
+    pass
